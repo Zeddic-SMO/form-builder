180d499674caffeb43391843477bab12a7110d4c
Samuel Ortil (Zoracom - NG) <samuel@zoracom.com>
2024-08-12 17:04:28 +0100

    Frustration.....

---
+++ src/component/ConfigElement.tsx
@@ -0,0 +1,5 @@
+const ConfigElement = () => {
+  return <div className="p-4 rounded-md">ConfigElement</div>;
+};
+
+export default ConfigElement;
--- src/component/FormBuilderEditor.tsx
+++ src/component/FormBuilderEditor.tsx
@@ -6,6 +6,7 @@ import { TemplateType } from "../types/FormTemplateTypes";
 import { FormItemTypes } from "../utils/formBuilder.utils";
 import DropContainerComponent from "./DropContainerComponent";
 import { useAppGlobalContext } from "../context";
+import ConfigElement from "./ConfigElement";
 
 interface FormBuilderEditorProps {
   template: TemplateType;
@@ -24,30 +25,35 @@ const FormBuilderEditor: FC<FormBuilderEditorProps> = (props) => {
   }, []);
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div>
-        <div className="w-[50rem] mx-auto my-10">
-          {allFormLayoutComponents &&
-            allFormLayoutComponents.map((layout, ind) => {
-              return (
-                <DropContainerComponent
-                  key={layout && layout.container.id}
-                  index={ind}
-                  layout={layout && layout.container}
-                  handleItemAdded={handleItemAdded}
-                  // selectedControl={selectedControl}
-                  childrenComponents={layout && layout.children}
-                  deleteContainer={deleteContainer}
-                  // deleteControl={deleteControl}
-                  // selectControl={selectControl}
-                  accept={FormItemTypes.CONTROL}
-                  // moveControl={moveControl}
-                />
-              );
-            })}
+    <div className="flex  gap-14 justify-end">
+      <DndProvider backend={HTML5Backend}>
+        <div>
+          <div className="w-[50rem] mx-auto my-10">
+            {allFormLayoutComponents &&
+              allFormLayoutComponents.map((layout, ind) => {
+                return (
+                  <DropContainerComponent
+                    key={layout && layout.container.id}
+                    index={ind}
+                    layout={layout && layout.container}
+                    handleItemAdded={handleItemAdded}
+                    // selectedControl={selectedControl}
+                    childrenComponents={layout && layout.children}
+                    deleteContainer={deleteContainer}
+                    // deleteControl={deleteControl}
+                    // selectControl={selectControl}
+                    accept={FormItemTypes.CONTROL}
+                    // moveControl={moveControl}
+                  />
+                );
+              })}
+          </div>
         </div>
+      </DndProvider>
+      <div className="w-[15rem] border-2 my-10 mr-2 rounded-md">
+        <ConfigElement />
       </div>
-    </DndProvider>
+    </div>
   );
 };
 
